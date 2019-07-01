77855c843bac1645dea985e4f5be541d6120bab9
brunneis <dev@brunneis.com>
2019-07-01 08:55:05 +0200

    Updated to 0.4.0.5 (stable)

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export TOR_VERSION=0.3.5.7
-export OR_PORT="9001 [2001:DB8::1]:9001"
-export DIR_PORT=9030
-export NICKNAME=NotProvided
-export CONTACT_INFO=not-provided@example.com
+export TOR_VERSION="0.4.0.5"
+export OR_PORT="9001"
+export DIR_PORT="9030"
+export NICKNAME="NotProvided"
+export CONTACT_INFO="not-provided@example.com"
 export BANDWIDTH_RATE="250 KBytes"
 export BANDWIDTH_BURST="500 KBytes"
 export MAX_MEM="512 MB"
-export ACCOUNTING_MAX=0
+export ACCOUNTING_MAX="0"
 export ACCOUNTING_START="month 1 00:00"
